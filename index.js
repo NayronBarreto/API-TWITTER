cbc2f03c8c709579503de604719bb2636fefac1a
Nayron Barreto <nayronbarreto@gmail.com>
2018-08-03 00:15:13 +0200

    User model created

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const app = require('express')();
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const dbConfig = require('./config/database');
 
 mongoose.connect(dbConfig.url);
+requireDir(dbConfig.modelPath);
 
 app.listen(3000);
